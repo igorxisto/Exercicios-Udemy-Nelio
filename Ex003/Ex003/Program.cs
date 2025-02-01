98c5ebd8547727c44959394944aca8e2f88ee18a
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2025-01-31 23:07:20 -0300

    Exercício 3

---
+++ Ex003/Ex003/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Ex003 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um número : ");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número : ");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número : ");
+            int C = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número : ");
+            int D = int.Parse(Console.ReadLine());
+            int Result = ((A * B) - (C * D));
+
+            Console.WriteLine($"A diferença é : {Result}");
+
+        }
+
+    }
+}
