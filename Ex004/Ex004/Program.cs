72a99f55278eaf46c94f3ff55f8cfa9b1a4474a1
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2025-01-31 23:08:19 -0300

    Exercício 4

---
+++ Ex004/Ex004/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex004 {
+    class Program {
+        static void Main(string[] args){
+            Console.WriteLine("Digite o ID do funcionário, horas trabalhadas e o salário : ");
+            int id = int.Parse(Console.ReadLine());
+            double Work_hour = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double wage = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double result = (wage * Work_hour); 
+
+            Console.WriteLine($"Funcionário : {id}");
+            Console.WriteLine($"Salary : U$ {result:F2}",CultureInfo.InvariantCulture);
+           
+        }
+    }
+}
