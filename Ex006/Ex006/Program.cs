c1317dc7f3c5cbc5f14c840a280404cc0f3d1008
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2025-02-01 01:24:16 -0300

    Exercício 6

---
+++ Ex006/Ex006/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Intrinsics;
+namespace Ex006 {
+    class Program {
+        static void Main(string[] args) {
+            string[] V = Console.ReadLine().Split(' ');
+            double A = double.Parse(V[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(V[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(V[2], CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+
+            //a) 
+            double tri = ((A * C) / (2.0));
+            //b)
+            double cir = (Math.Pow(C, 2)) * pi;
+            //c)
+            double trap = ((A + B) * C) / 2.0;
+            //d)
+            double quad = (Math.Pow(B, 2));
+            //e)
+            double ret = A * B;
+
+
+            Console.WriteLine("TRIANGULO: " + tri.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + cir.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trap.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quad.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + ret.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
