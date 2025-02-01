5361c55930090c9a9fd7ad0538c20b7b3ac593c4
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2025-01-31 23:05:25 -0300

    Exercício 2

---
+++ Ex002/Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Globalization; 
+
+namespace Ex002 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite o valor do Raio de um circulo : ");
+            double ray  = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Loading...");
+            Double result = ((3.14159) * Math.Pow(ray, 2));
+            Console.WriteLine(result.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+};
